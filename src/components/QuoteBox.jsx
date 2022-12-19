77b98cfcf2f5a48fbe862c68ba90f3ea53e1f13f
Miguel1488 <118023734+Miguel1488@users.noreply.github.com>
2022-12-19 12:15:43 -0500

    Se agrega el proyecto

---
+++ src/components/QuoteBox.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import quotes from '../quotes.json';
+
+const QuoteBox = () => {
+  console.log(quotes);
+
+  const random = Math.floor(Math.random() * quotes.length);
+  const [index, setIndex] = useState(random);
+  let colorRandom = Math.floor(Math.random() * 16777215).toString(16);
+  const [color, setColor] = useState("E2202B");
+  const newRandom = Math.floor(Math.random() * quotes.length);
+
+  const changeUser = () => {
+    setColor(colorRandom)
+    setIndex(newRandom);
+  };
+
+  return (
+    <div className="containerGeneral" style={{ background: `#${color}` }}>
+      <div className="user-box">
+        <section className="phrase"> <i style={{ color: `#${color}` }} className="fa-solid fa-quote-left"></i>
+          <h1 className="text" style={{ color: `#${color}` }}>{quotes[index].quote}</h1>
+        </section>
+
+        <section className="containerAuthor">
+          <p className="author" style={{ color: `#${color}` }}>{quotes[index].author}</p>
+        </section>
+
+        <section className="button">
+          <i style={{ color: `#${color}` }} onClick={changeUser} className="fa-sharp fa-solid fa-circle-chevron-right"></i>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default QuoteBox;
